74253998cc1d3088cd438e3d3fb07a9c457f9a96
damienn7 <damien.nicolleau@epitech.eu>
2025-01-20 15:30:51 +0100

    Added attr and method getter and setter to Base, Cream and Topping class

--- src/main/java/org/example/Base.java
+++ src/main/java/org/example/Base.java
@@ -1,4 +1,33 @@
 package org.example;
 
 public class Base {
+    private Float price;
+
+    private Integer number;
+
+    private String name;
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
